8b5e6eef70d49d2e9f255994bb8b841dd09d3100
Robert Wagner <rwwagner90@gmail.com>
2018-05-29 17:39:34 -0400

    First attempt

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const StaticSiteJson = require('broccoli-static-site-json');
+
 module.exports = {
-  name: 'ember-cli-markdown-to-json'
+  name: 'ember-cli-markdown-to-json',
+  treeForPublic() {
+    this._super.treeForPublic && this._super.treeForPublic.apply(this, arguments);
+
+    return new StaticSiteJson('blog', {
+      attributes: [
+        'author',
+        'authorId',
+        'categories',
+        'date',
+        'slug',
+        'title'
+      ],
+      collections: [{
+        src: 'blog',
+        output: 'blog.json'
+      }]
+    });
+  }
 };
