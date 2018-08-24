da1df4dfa3c45eeb33ba1e9d1a6bc6c046fe2760
Robert Wagner <rwwagner90@gmail.com>
2018-08-24 11:48:15 -0400

    Support multiple trees

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const MergeTrees = require('broccoli-merge-trees');
 const StaticSiteJson = require('broccoli-static-site-json');
 
 module.exports = {
@@ -8,13 +9,22 @@ module.exports = {
     this._super.treeForPublic && this._super.treeForPublic.apply(this, arguments);
 
     if (this.app.options && this.app.options['ember-cli-markdown-to-json']) {
-      const { attributes, collections, contentTypes, folder } = this.app.options['ember-cli-markdown-to-json'];
+      const options = this.app.options['ember-cli-markdown-to-json'];
 
-      return new StaticSiteJson(folder, {
-        attributes,
-        collections,
-        contentTypes: contentTypes || ['content', 'description', 'html']
+      const trees = options.map((option) => {
+        const { attributes, collections, contentFolder, contentTypes, folder, references, type } = option;
+
+        return new StaticSiteJson(folder, {
+          attributes,
+          collections,
+          contentFolder: contentFolder || 'content',
+          contentTypes: contentTypes || ['content', 'description', 'html'],
+          references,
+          type
+        });
       });
+
+      return MergeTrees(trees);
     }
   }
 };
